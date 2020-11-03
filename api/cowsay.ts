41be7e573a852b87a5a88982c9b57ad9e4d414c0
Lachlan Campbell <lachlan@hackclub.com>
2020-11-03 13:57:46 -0500

    Simplify syntax slightly

--- api/cowsay.ts
+++ api/cowsay.ts
@@ -2,13 +2,13 @@ import cowsay, { TURTLE } from "cowsay";
 import { NowRequest, NowResponse } from "@vercel/node";
 
 export default (req: NowRequest, res: NowResponse) => {
-  let text = req.body.text;
-  let turtleText = "-f turtle";
+  const { text } = req.body;
+  const turtleText = "-f turtle";
   res.json({
     response_type: "in_channel",
     text:
       "```" + text.includes(turtleText)
-        ? cowsay.say({ text: text })
+        ? cowsay.say({ text })
         : cowsay.say({ text: text.trim(turtleText).trim(" "), cow: TURTLE }) +
           "```",
   });
