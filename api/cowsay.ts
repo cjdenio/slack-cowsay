d63754291a69ac3e511970c67d2ed6c7b127df81
Caleb Denio <cjdenio44@gmail.com>
2020-11-03 14:13:38 -0500

    AHHHH

--- api/cowsay.ts
+++ api/cowsay.ts
@@ -1,15 +1,17 @@
-import cowsay, { TURTLE } from "cowsay";
+import cowsay from "cowsay";
 import { NowRequest, NowResponse } from "@vercel/node";
 
 export default (req: NowRequest, res: NowResponse) => {
   let text = req.body.text;
   let turtleText = "-f turtle";
+
   res.json({
     response_type: "in_channel",
     text:
-      "```" + text.includes(turtleText)
-        ? cowsay.say({ text: text })
-        : cowsay.say({ text: text.trim(turtleText).trim(" "), cow: TURTLE }) +
-          "```",
+      "```" +
+      (text.includes(turtleText)
+        ? cowsay.say({ text: text, f: "turtle" })
+        : cowsay.say({ text: text })) +
+      "```",
   });
 };
