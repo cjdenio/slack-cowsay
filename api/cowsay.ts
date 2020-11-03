bd3343f0d5f1374dc62e30cde34ef68518de3ce3
Matthew Gleich <email@mattglei.ch>
2020-11-03 13:38:33 -0500

    ✨ Add turtle support
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- api/cowsay.ts
+++ api/cowsay.ts
@@ -1,14 +1,15 @@
-import cowsay from "cowsay";
-import { NowRequest, NowResponse } from "@vercel/node";
+import { cowsay, TURTLE } from 'cowsay';
+import { NowRequest, NowResponse } from '@vercel/node';
 
 export default (req: NowRequest, res: NowResponse) => {
+  let text = req.body.text;
+  let turtleText = '-f turtle';
   res.json({
-    response_type: "in_channel",
+    response_type: 'in_channel',
     text:
-      "```" +
-      cowsay.say({
-        text: req.body.text,
-      }) +
-      "```",
+      '```' + text.includes(turtleText)
+        ? cowsay.say({ text: text })
+        : cowsay.say({ text: text.trim(turtleText).trim(' '), cow: TURTLE }) +
+          '```',
   });
 };
